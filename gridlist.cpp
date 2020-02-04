34c01c8f4e3e605f743646afcfc7a824630b6548
UBC Student <arya2306@valdes.students.cs.ubc.ca>
2020-02-03 22:18:09 -0800

    WOrking on checkerswap

--- gridlist.cpp
+++ gridlist.cpp
@@ -132,7 +132,6 @@ void GridList::Sandwich_V(GridList& inner)
         inner.northwest->prev = curr;
         inner.southeast->next = after;
         after->prev = inner.southeast;
-
       }
     }
     dimy += inner.dimy;
@@ -158,40 +157,48 @@ void GridList::CheckerSwap(GridList& otherlist)
 
   //still gotta check for same pixel dimension
   if (curr->data.Dimension() == other->data.Dimension() && dimx == otherlist.dimx && dimy == otherlist.dimy) {
-    CheckerSwapHelper(otherlist);
-    otherlist.CheckerSwapHelper(*this);
-  }
-}
+    GridNode* currNodeThis = northwest;
+    GridNode* currNodeOther = otherlist.northwest;
+    GridNode* otherAfter = otherlist.northwest;
 
-void GridList::CheckerSwapHelper(GridList& otherlist)
-{
-  GridNode* currNodeThis = northwest;
-  GridNode* currNodeOther = otherlist.northwest;
+    for (int i = 0; i < dimy; i++) {
+      GridNode* before = northwest;
+      for (int j = 0; j < dimx; j++) {
+        if (i % 2 == 0) {
+          if (j % 2 == 1) {
+            otherAfter = currNodeOther->next;
+            before = currNodeThis->prev;
+            currNodeOther->prev->next = currNodeThis;
+            currNodeThis->prev = currNodeOther->prev;
+            currNodeOther->prev = before;
+            // SEG FAULT AT THIS LINE I HAVE NO CLUE WHY
+            // currNodeThis->next->prev = currNodeOther;
+            currNodeOther->next = currNodeThis->next;
+            currNodeThis->next = otherAfter;
+            before->next = currNodeOther;
+            if (currNodeThis->next != NULL) currNodeThis->next->prev = currNodeThis;
 
-  for (int i = 0; i < dimy; i++) {
-    GridNode* before = northwest;
-    for (int j = 0; j < dimx; j++) {
-      if (i % 2 == 0) {
-        if (j % 2 == 1) {
-          before = currNodeThis->prev;
-          currNodeOther->prev = before;
-          currNodeOther->next = currNodeThis->next;
-          before->next = currNodeOther;
-          currNodeThis = currNodeOther;
-          if (currNodeThis->next != NULL) currNodeThis->next->prev = currNodeOther;
+          }
+        } else {
+          if (j % 2 == 0) {
+            otherAfter = currNodeOther->next;
+            before = currNodeThis->prev;
+            currNodeOther->prev->next = currNodeThis;
+            // currNodeThis->prev = currNodeOther->prev;
+            currNodeOther->prev = before;
+            currNodeThis->next->prev = currNodeOther;
+            currNodeOther->next = currNodeThis->next;
+            currNodeThis->next = otherAfter;
+            before->next = currNodeOther;
+            if (currNodeThis->next != NULL) currNodeThis->next->prev = currNodeThis;
+
+          }
         }
-      } else {
-        if (j % 2 == 0) {
-          before = currNodeThis->prev;
-          currNodeOther->prev = before;
-          currNodeOther->next = currNodeThis->next;
-          before->next = currNodeOther;
-          currNodeThis = currNodeOther;
-          if (currNodeThis->next != NULL) currNodeThis->next->prev = currNodeOther;
+        if (currNodeThis->next != NULL) {
+          currNodeThis = currNodeThis->next;
+          currNodeOther = currNodeOther->next;
         }
       }
-      currNodeThis = currNodeThis->next;
-      currNodeOther = currNodeOther->next;
     }
   }
 }
